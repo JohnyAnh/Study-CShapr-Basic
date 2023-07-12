cd2860b989587139881304eee56ca6aa6475c354
JohnyAnh <112863282+JohnyAnh@users.noreply.github.com>
2023-07-12 14:27:33 +0700

    Arrays, iterate through each item in the sequence.

---
+++ ExP2ForArray/Program.cs
@@ -0,0 +1,8 @@
+﻿String[] Arr = {"B123","C234","A345","C15","B177","G3003","C235","B179"};
+foreach (String item in Arr)
+{
+    if (item.StartsWith("B"))
+    {
+        Console.WriteLine($"The name starts with {item}!");
+    }
+}
