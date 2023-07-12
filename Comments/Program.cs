bb4a1b2b1433b7c5eb89fdc62041395d19bc750a
JohnyAnh <112863282+JohnyAnh@users.noreply.github.com>
2023-07-12 16:25:05 +0700

    conventions, whitespace, and comments in C#

---
+++ Comments/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Notice that there are two main problems with these comments:
+
+The code comments unnecessarily explain the obvious functionality of individual lines of code. These are considered 
+low-quality comments because they merely explain how C# or methods of the .NET Class Library work. 
+If the reader is unfamiliar with these ideas, they can look them up using learn.microsoft.com or IntelliSense.
+
+The code comments don't provide any context to the problem being solved by the code. These are considered low-quality 
+comments because the reader doesn't gain any insight into the purpose of this code, especially as it relates to the 
+larger system.
+*/
+
+
+/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+*/
+Random random = new Random();
+string[] orderIDs = new string[5];
+
+for (int i = 0; i < orderIDs.Length; i++)
+{
+    int prefixValue = random.Next(65, 70);
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
+
+    orderIDs[i] = prefix + suffix;
+}
+
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+}
---
+++ whitespace/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+What is whitespace?
+The term "whitespace" refers to individual spaces produced by the space bar, tabs produced by the tab key, 
+and new lines produced by the enter key.
+
+The C# compiler ignores whitespace. To understand how whitespace is ignored, and how to maximize clarity using white space, 
+work through the following exercise.
+*/
+
+// Random dice = new Random();
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
+// int total = roll1 + roll2 + roll3;
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
+//     if ((roll1 == roll2) && (roll2 == roll3)) {
+//         Console.WriteLine("You rolled triples!  +6 bonus to total!");
+//         total += 6; 
+//     } else {
+//         Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+//         total += 2;
+//     }
+// }
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) 
+{
+    if ((roll1 == roll2) && (roll2 == roll3)) 
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6; 
+    } 
+    else 
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+}
+
+
+/*
+The main takeaways from this exercise:
+
+Use whitespace judiciously to improve the readability of your code.
+Use line feeds to create empty lines to separate phrases of code. A phrase includes lines of code that are similar, or work together.
+Use line feeds to separate code block symbols so that they are on their own line of code.
+Use the tab key to line up a code block with the keyword they're associated with.
+Indent code inside of a code block to show ownership.
+*/
