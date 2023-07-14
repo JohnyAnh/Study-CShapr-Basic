ff3708d24a0027fcee188f326eebf15b02a02cf6
JohnyAnh <112863282+JohnyAnh@users.noreply.github.com>
2023-07-14 15:34:30 +0700

    Conditional operator

---
+++ EvaluateAnExpression/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("a" == "a");
+Console.WriteLine("a" == "A");
+Console.WriteLine(1 == 2);
+Console.WriteLine("\n");
+string myValue = "a";
+Console.WriteLine(myValue == "a");
+string value1 = " a";
+string value2 = "A ";
+Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+Console.WriteLine("\n");
+Console.WriteLine("a" != "a");
+Console.WriteLine("a" != "A");
+Console.WriteLine(1 != 2);
+
+string myValue1 = "a";
+Console.WriteLine(myValue1 != "a");
+Console.WriteLine("\n");
+Console.WriteLine(1 > 2);
+Console.WriteLine(1 < 2);
+Console.WriteLine(1 >= 1);
+Console.WriteLine(1 <= 1);
+Console.WriteLine("\n");
+
+// If have "fox" = true else = false
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow"));
+
+// These two lines of code will create the same output
+
+Console.WriteLine(pangram.Contains("fox") == false);
+Console.WriteLine(!pangram.Contains("fox"));
+
+// Logical Negation operator
+Console.WriteLine(!pangram.Contains("fox"));
+Console.WriteLine(!pangram.Contains("cow"));
