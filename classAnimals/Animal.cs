2f2f02e5f996079c6cdd2e4ab59be149620a548b
JohnyAnh <112863282+JohnyAnh@users.noreply.github.com>
2023-07-19 09:48:35 +0700

    Exercise-Buildand new pet and Class

---
+++ classAnimals/Animal.cs
@@ -0,0 +1,7 @@
+public class animals
+{
+    private String soundAnimal;
+    public int countLeg = 2;
+    public int CountLeg { get; private set; } = 5; // private set thì bên class khác chỉ đọc không sửa
+    public int MyProperty { get; set; }
+}
---
+++ classAnimals/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AppName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            animals dog = new animals();
+            Console.WriteLine(dog.CountLeg + dog.countLeg);
+            // dog.CountLeg = 50; private set nên không sửa đc
+        }
+    }
+}
