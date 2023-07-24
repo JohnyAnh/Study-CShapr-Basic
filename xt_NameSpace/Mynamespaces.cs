349dc661772b8dde79da8d959bf709905d80c70b
Johny Vu <johnyanhn01@gmail.com>
2023-07-24 11:01:37 +0700

    XT-namespace

---
+++ xt_NameSpace/Mynamespaces.cs
@@ -0,0 +1,22 @@
+namespace MyNamespace
+{
+    class Class1
+    {
+        public static void Xinchao()
+        {
+            Console.WriteLine("Hello my class1");
+        }
+    }
+
+    namespace MyNamespace2
+    {
+        class class1
+        {
+            public static void Xinchao()
+            {
+                Console.WriteLine("Hello my MyNamespace2 class1");
+            }
+        }
+
+    }
+}
---
+++ xt_NameSpace/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyNamespace.Class1.Xinchao();
+            MyNamespace.MyNamespace2.class1.Xinchao();
+        }
+    }
+}
