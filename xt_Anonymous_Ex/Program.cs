1a0ff42100ffbb2bb1b0ec7173c0ab0713e3b77a
Johny Vu <johnyanhn01@gmail.com>
2023-07-24 16:43:22 +0700

    Anonymous- Tao obj truy van Linq

--- xt_Anonymous_Ex/Program.cs
+++ xt_Anonymous_Ex/Program.cs
@@ -1,10 +1,6 @@
 ﻿namespace Anonymoust
 {
-    class Program
-    {
-        public static void Main(string[] args)
-        {
-            /*Cu Phap
+    /*Cu Phap
             var obj = new 
             {
                 thuoctinh1 = giatri1,
@@ -17,15 +13,40 @@
 
             Luu y: Anonymous chi dung de doc
             */
-
-            var myProfire = new
-            {
-                name = "Johny",
-                age = 26,
-                adress = "Ha Noi"
+    class Student
+    {
+        public string nameStudent { get; set; }
+        public int yearStudennt { get; set; }
+        public string adressStudent { get; set; }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>() {
+                new Student() {nameStudent= "Nam", yearStudennt = 2000, adressStudent = "Binh Duong"},
+                new Student() {nameStudent= "Mo", yearStudennt = 2002, adressStudent = "Nam Dinh"},
+                new Student() {nameStudent= "Adi", yearStudennt = 2001, adressStudent = "Vinh Phuc"},
+                new Student() {nameStudent= "DaPhat", yearStudennt = 2000, adressStudent = "Nam Dinh"}
             };
+            var result = from st in students
+                             //  where st.yearStudennt <= 2001
+                             //  where st.nameStudent == "Adi"
+                             //   where st.nameStudent.Contains("a")//Tim theo ten co chu a
+                         where st.adressStudent == "Nam Dinh"
+                         select new
+                         {
+                             nameOf = st.nameStudent,
+                             yearOf = st.yearStudennt,
+                             adressOf = st.adressStudent
+                         };
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.nameOf + " - " + item.yearOf + " - " + item.adressOf);
+            }
 
-            Console.WriteLine(myProfire.name);
+
+
 
         }
     }
