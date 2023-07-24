f7ee7d0687637c24b7df5a9f6714fa6962b4632b
Johny Vu <johnyanhn01@gmail.com>
2023-07-24 16:20:10 +0700

    Anonymous- Kieu Vo Danh

---
+++ xt_Anonymous_Ex/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Anonymoust
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*Cu Phap
+            var obj = new 
+            {
+                thuoctinh1 = giatri1,
+                thuoctinh2 = giatri2
+            };
+
+            Để truy cập thuộc tính của toán tử vẫn dùng ký hiệu . và tên thuộc tính.
+
+            Console.WriteLine(myProfile.name);
+            */
+
+            var myProfire = new
+            {
+                name = "Johny",
+                age = 26,
+                adress = "Ha Noi"
+            };
+
+            Console.WriteLine(myProfire.name);
+
+        }
+    }
+}
