bce6e90a1c1df242449ada05a77674f8aeb60917
JohnyAnh <112863282+JohnyAnh@users.noreply.github.com>
2023-07-25 08:38:11 +0700

    Virtual,  Hàm ảo, nạp trông phương thức override

---
+++ xt_VirtualMethod_HamAo/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Virtual
+{
+    class Product
+    {
+        protected double Price { get; set; }
+
+        // Phương thức ảo ProductInfo
+        public virtual void productInfor()
+        {
+            Console.WriteLine($"Gia san pham {Price}");
+        }
+
+        public void Test() => productInfor();
+    }
+
+    class Iphone : Product
+    {
+        public Iphone() => Price = 500;
+
+        // override - nap chong phuong thuc tai lop ke thua khai phuong thuc y het. thay virtual = override
+        public override void productInfor()
+        {
+            Console.WriteLine("Iphone 15");
+            // base.productInfor(); //them base nếu muốn dùng phương thưc nguyên bản chưa bị ghi đè override
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Iphone iphone = new Iphone();
+            iphone.Test();
+        }
+    }
+}
