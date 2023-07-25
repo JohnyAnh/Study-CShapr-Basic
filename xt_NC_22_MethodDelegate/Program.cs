1d9918b0025cd5e31fe6b3c1fd14b4e554645417
JohnyAnh <112863282+JohnyAnh@users.noreply.github.com>
2023-07-25 15:45:52 +0700

    Khái niệm, khai báo , gán, thi hành delegate

---
+++ xt_NC_22_MethodDelegate/Program.cs
@@ -0,0 +1,47 @@
+﻿// delegate (type) biến đc gán bằng phương thức, khai báo ở trong namespace hoặc trong class
+namespace Delegate
+{
+    //1. Khai báo giống như method thông thường thêm từ khóa delegate thêm kiểu trả về
+    public delegate void Showlog(string message);
+
+    class Program
+    {
+        static void Info(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(s);
+            Console.ResetColor();
+        }
+
+        static void Warning(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(s);
+            Console.ResetColor();
+        }
+
+        public static void Main(string[] args)
+        {
+            Showlog log = null;
+
+            /*
+            */
+            log = Info;
+            //Kiểm tra biến log khác null
+            if (log != null)
+                log("Xin Chao");
+
+            log?.Invoke("Xin chao Abc"); //Cách viết giống dùng if
+
+            log = Warning;
+            log("Hoc ve delegate");
+            /*
+            //Cách 1 thi hành các biến đang lưu trong delagate
+            log = Info;
+            log("Xin Chao");
+            //Cách 2 thi hành các biến đang lưu trong delagate
+            log.Invoke("Xin chào Abc");
+            */
+        }
+    }
+}
