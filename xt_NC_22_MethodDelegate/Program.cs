29ebc6be2992e14b423dbbb8f2ad5e5bffaf8267
JohnyAnh <112863282+JohnyAnh@users.noreply.github.com>
2023-08-22 09:50:01 +0700

    Delegate, Func, sd Delegate trong phương thức

--- xt_NC_22_MethodDelegate/Program.cs
+++ xt_NC_22_MethodDelegate/Program.cs
@@ -20,9 +20,44 @@ namespace Delegate
             Console.ResetColor();
         }
 
+        /*
+        //Ví dụ Delegate Func
+                static int Tong(int a, int b) => a + b;
+        
+                static int Hieu(int a, int b) => a - b;
+        */
+
+
+        // Sử dung delegate trong tham số phương thức
+        static void Tong(int a, int b, Showlog log)
+        {
+            int kq = a + b;
+            log?.Invoke($"Tổng là {kq}");
+        }
+
         public static void Main(string[] args)
         {
-            // Nhiều phương thức(chuỗi phương thức)
+            //Delegate trong C#
+            //Action, FunC: delegate - gereric
+
+            //Delegate Func trong C# xác định kiểu trả về kiểu trả về được xác định ở cuối cùng. Nếu có các tham số thì tham số được liệt kê ở phía trước.
+            //Func<int> f1; //~delegate int KIEU()
+            //Func<string, double, string> f2; //~delegate string KIEU(string s, double s);
+
+            /*
+            //Ví dụ Delegate Func
+            Func<int, int, int> tinhToan; //~delegate int KIEU(int a, int b);
+            int a = 5,
+                b = 10;
+            tinhToan = Tong;
+            Console.WriteLine($"Tong la: {tinhToan(a, b)}");
+            */
+
+            // Sử dung delegate trong tham số phương thức
+            Tong(4, 5, Info); // In ra màu xanh
+            Tong(4, 5, Warning); // In ra màu đỏ
+
+            /* Nhiều phương thức(chuỗi phương thức)
             Showlog log = null; //+=
 
             //Dùng += để gọi các phương thức với delegate gọi bao lần hiện bấy nhiêu lần
@@ -33,7 +68,7 @@ namespace Delegate
             log += Warning;
 
             log?.Invoke("Xin chao");
-
+            */
             /*
             Showlog log = null;
             log = Info;
